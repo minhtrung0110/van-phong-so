0e700fc231c9cf6930bc244122696e4f444fd90c
minhtrung0110 <nguyenducminhtrung0044@gmail.com>
2023-02-08 00:54:49 +0700

    build Avatar-Upload-CropImg

--- src/components/Client/Profile/BoxAvatar/index.jsx
+++ src/components/Client/Profile/BoxAvatar/index.jsx
@@ -1,37 +1,76 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import {Col, Image} from "antd";
+import {Col, Image, Modal} from "antd";
 import Avatar from "react-avatar-edit";
 import './style.scss'
+import {FaCamera} from "react-icons/fa";
 BoxAvatar.propTypes = {
 
 };
 
 function BoxAvatar(props) {
-    const [src,setSrc]=useState()
-    const [preview,setPreview]=useState()
+    const [src,setSrc]=useState('http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcQTBIkxproxJHBsj2ZOkeFr3CYyVJjrfW8qcovw9whTrkRjsqYnBRlprpmyAknfOsug43oiT9iqS9cJe6s')
+    const [preview,setPreview]=useState(null)
+    const [isUpdateAvatar,setIsUpdateAvatar] = useState(false)
     const handleCrop=(view)=>{
       setPreview(view)
     }
     const handleClose=()=>{
         setPreview(null)
     }
-    const handleBeforeFileUpload=()=>{
+    const handleUpload=()=>{
+        !!preview && setSrc(preview)
+        setIsUpdateAvatar(false)
+    }
+    const handleCancelUpload=()=>{
+            setPreview(null)
+        setIsUpdateAvatar(false)
+    }
+    const handleOpenPopupUpload=()=>{
+        console.log(preview)
+        setPreview(null)
 
+
+        setIsUpdateAvatar(true)
+    }
+
+    const handleBeforeFileUpload=(elem)=>{
+        if (elem.target.files[0].size > 9971680) {
+            alert("File is too big!");
+            elem.target.value = "";
+        }
     }
     return (
         <Col className='box-avatar box' span={8}>
-            <Avatar
-                width={210}
-                height={170}
-                onCrop={handleCrop}
-                onClose={handleClose}
-                onBeforeFileLoad={handleBeforeFileUpload}
-                src={src}
-            />
-            {
-                !!preview &&   <img src={src} alt={preview}/>
-            }
+            <div className="container-img" onClick={handleOpenPopupUpload}>
+                <img src={src} className='img-avatar' />
+                    <div className="middle">
+                        <FaCamera className='icon-camera' />
+                    </div>
+            </div>
+
+            <Modal
+                title="Tải Ảnh Đại Diện"
+                open={isUpdateAvatar}
+                onCancel={handleCancelUpload}
+                onOk={handleUpload}
+                okText="Cập Nhật"
+                cancelText="Hủy"
+                okButtonProps={{
+                    disabled: !!!preview,
+                }}
+            >
+                <Avatar
+                    width={210}
+                    height={170}
+                    onCrop={(view)=>setPreview(view)}
+                    onClose={()=>setPreview(null)}
+                    onBeforeFileLoad={handleBeforeFileUpload}
+                    src={null}
+
+                />
+            </Modal>
+
         </Col>
     );
 }
