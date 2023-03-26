bfe53828d50865e44b41209369d6fa0671dcf160
minhtrung0110 <nguyenducminhtrung0044@gmail.com>
2023-03-27 02:11:21 +0700

    build Schedule Page

--- src/components/Client/Schedule/EventItem/index.jsx
+++ src/components/Client/Schedule/EventItem/index.jsx
@@ -1,13 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
+import {useForm, Controller} from "react-hook-form";
+import {Form, Input} from "antd";
+import './style.scss'
+import {FaCalendar, FaCalendarAlt, FaCheck, FaCheckCircle, FaMoneyCheckAlt, FaUserClock} from "react-icons/fa";
 
-EventItem.propTypes = {
+EventItem.propTypes = {};
 
-};
-
-function EventItem(props) {
+function EventItem({event}) {
+    const [typeEvent, setTypeEvent] = useState('event')
+    const {
+        control, handleSubmit, formState: {errors, isDirty, dirtyFields},
+    } = useForm({});
+    const onSubmit = (data) => {
+        console.log(data)
+    }
     return (
-        <div></div>
+        <Form
+
+            layout="horizontal"
+            style={{}}
+            onFinish={handleSubmit(onSubmit)}
+            labelAlign={"left"}
+            className='event-item'
+        >
+            <div className='type-event-item'>
+                <span className={`type-item event ${typeEvent === 'event' ? 'active' : ''}`}
+                      onClick={() => setTypeEvent('event')}
+                >
+                   <FaCalendarAlt className='icon'/> Sự kiện</span>
+                <span className={`type-item schedule ${typeEvent === 'schedule' ? 'active' : ''}`}
+                      onClick={() => setTypeEvent('schedule')}
+                >
+                <FaUserClock
+                    className='icon'/> Lịch trình</span>
+                <span className={`type-item todo ${typeEvent === 'todo' ? 'active' : ''}`}
+                      onClick={() => setTypeEvent('todo')}
+                ><FaCheckCircle
+                    className='icon'/> Cần làm</span>
+            </div>
+            <Controller
+                name="title"
+                control={control}
+                defaultValue=""
+                rules={{required: true}}
+                render={({field}) => (
+                    <Form.Item
+
+                        hasFeedback
+                        help={errors.name ? 'Vui lòng nhập tên dự án' : null}>
+                        <Input
+                            {...field} className='input-title-event' size="large"
+                            placeholder="Thêm tiêu đề cho sự kiện này."/>
+                    </Form.Item>
+                )}
+            />
+        </Form>
     );
 }
 
--- src/pages/Client/ManageSchedule/index.jsx
+++ src/pages/Client/ManageSchedule/index.jsx
@@ -1,23 +1,167 @@
-import React from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 import PropTypes from 'prop-types';
-import {Form, InputGroup} from 'react-bootstrap'
-
+import './style.scss'
+import {Calendar, momentLocalizer, Views} from "react-big-calendar";
+import moment from "moment";
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import {Modal} from "antd";
+import EditProject from "~/components/Client/Project/EditProject";
+import EventItem from "~/components/Client/Schedule/EventItem";
 ManageSchedule.propTypes = {
 
 };
 
 function ManageSchedule(props) {
-    return (
-        <div>
-            <InputGroup className="mb-3">
-                <Form.Control
-                    placeholder="Recipient's username"
-                    aria-label="Recipient's username"
-                    aria-describedby="basic-addon2"
-                />
-                <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
-            </InputGroup>
+    const [myEvents, setEvents] = useState([])
+    const [showEvent,setShowEvent] = useState({event:null,show:false})
+    const handleCancelShowEvent = ()=>{
+        setShowEvent({...showEvent,show: false})
+    }
+    const events = [
+        {
+            title: 'Sự kiện A',
+            start: new Date(),
+            end: new Date(),
+            description: 'Mô tả sự kiện A'
+        },
+        {
+            title: 'Họp Dự Án ',
+            type: 'event',
+            description: '',
+            file:'',
+            members: [],
+            notifications:true,
+            repeats:true,
+            start: new Date(),
+            end: new Date(),
+        },
+        {
+            title: 'Sự kiện B',
+            start: new Date(),
+            end: new Date(),
+            description: 'Mô tả sự kiện B'
+        },
+        {
+            title: 'Sự kiện lặp lại',
+            start: new Date(2023, 2, 1, 10, 0), // Ngày bắt đầu
+            end: new Date(2023, 2, 12, 12, 0), // Ngày kết thúc
+            rrule: {
+                freq: 'weekly',
+                interval: 1,
+                byweekday: [moment.weekdays().indexOf('Wednesday')],
+                dtstart: new Date(2023, 2, 1),
+                until: new Date(2023, 4, 1),
+            },
+        },
+        // ...
+    ];
 
+    // const events = [
+    //     {
+    //         title: 'Họp Dự Án ',
+    //         type: 'event',
+    //         description: '',
+    //         file:'',
+    //         members: [],
+    //         notifications:true,
+    //         repeats:true,
+    //         start: moment('2023-02-25 09:00:00'),
+    //         end: moment('2023-02-25 10:00:00'),
+    //     },
+    //     {
+    //         title: 'Hoàn Thành Lịch Biểu ',
+    //         type: 'todo',
+    //         description: '',
+    //         notifications:true,
+    //         repeats:true,
+    //         start: moment('2023-02-25 09:00:00'),
+    //         end: moment('2023-02-25 10:00:00'),
+    //     },
+    //     {
+    //         title: 'Đóng Tiền Nước ',
+    //         type: 'reminder',
+    //         notifications:true,
+    //         repeats:true,
+    //         start: moment('2023-02-25 09:00:00'),
+    //         end: moment('2023-02-25 10:00:00'),
+    //     },
+    // ];
+    const localizer = momentLocalizer(moment);
+
+    const handleSelectSlot = useCallback(
+        ({ start, end }) => {
+            const title = window.prompt('New Event Name')
+            if (title) {
+                setEvents((prev) => [...prev, { start, end, title, allDay:true }])
+            }
+        },
+        [setEvents]
+    )
+
+    const handleSelectEvent = useCallback(
+        (event) => {
+            setShowEvent({event: event,show: true})
+            console.log(event)
+        },
+        []
+    )
+    const { defaultDate, scrollToTime } = useMemo(
+        () => ({
+            defaultDate: new Date(2023, 2, 12),
+            scrollToTime: new Date(2023, 1, 1, 6),
+        }),
+        []
+    )
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        const style = {
+            backgroundColor: event.color,
+            borderRadius: '5px',
+            opacity: 0.8,
+            color: 'white',
+            border: '0px',
+            display: 'block',
+            width:'100%',
+            verticalAlign: 'middle'
+        };
+        return {
+            style: style,
+            title: event.title,
+            type:event.type,
+            description: event.description,
+            files:event.files,
+            members:event.members,
+            notification:event.notification,
+            repeats:event.repeat,
+        };
+    };
+    return (
+        <div  className='container-schedule'>
+            <Calendar
+                className='calendar'
+                defaultDate={defaultDate}
+                defaultView={Views.MONTH}
+                events={events}
+                localizer={localizer}
+                onSelectEvent={handleSelectEvent}
+                onSelectSlot={handleSelectSlot}
+                selectable
+                scrollToTime={scrollToTime}
+                eventPropGetter={eventStyleGetter}
+                recurringEvents={events}
+            />
+            <Modal
+                title="Sư Kiện"
+                onCancel={handleCancelShowEvent}
+                footer={null
+                }
+                width={600}
+                style={{ top: 100   }}
+                bodyStyle={{height: "500px"}}
+
+                open={showEvent.show}
+            >
+                <EventItem event={showEvent.event} />
+            </Modal>
         </div>
     );
 }
